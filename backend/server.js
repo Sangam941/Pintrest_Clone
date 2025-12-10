592b0269f2c85999d125b0f798ad1bad6ee78dd6
Sangam Singh Dhami <sangamdhami663@gmail.com>
2025-12-10 18:53:19 +0545

    cors update

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use(cors({
   origin: ['http://localhost:5173',
     'https://pintrest-clone-seven.vercel.app/'
   ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 
